306666545adcfd3625b87893f2954b24b9f49ec6
arnyarny <ucab.arny1@gmail.com>
2025-04-16 09:52:18 +0800

    get items

---
+++ api/get_items.php
@@ -0,0 +1,22 @@
+<?php
+include '../db/db.php';
+header('Content-Type: application/json');
+
+try {
+    $sql = "SELECT item_id, name FROM items ORDER BY name ASC";
+    $result = $conn->query($sql);
+
+    if (!$result) {
+        throw new Exception("SQL Error: " . $conn->error);
+    }
+
+    $items = [];
+    while ($row = $result->fetch_assoc()) {
+        $items[] = $row;
+    }
+
+    echo json_encode(["success" => true, "data" => $items]);
+} catch (Exception $e) {
+    echo json_encode(["success" => false, "message" => $e->getMessage()]);
+}
+?>
